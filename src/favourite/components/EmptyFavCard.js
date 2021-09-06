1af590b5e9d92b8f99b3e14d2fada71381d33bd5
timmygunja <t.urusov13@mail.ru>
2021-09-06 13:09:29 +0300

    added empty favourites styling

--- src/favourite/components/EmptyFavCard.js
+++ src/favourite/components/EmptyFavCard.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import classes from "./EmptyCartCard.module.css";
+import classes from "./EmptyFavCard.module.css";
 
-const EmptyCartCard = (props) => {
+const EmptyFavCard = (props) => {
   return (
     <Link to={"/"} className={classes.product}>
       <div className={classes["prod-pic"]}>
         <img src={"product.png"} alt="" />
       </div>
       <div className={classes["prod-content"]}>
-        <div className={classes["prod-title"]}>No products in cart found</div>
+        <div className={classes["prod-title"]}>No products in favourites found</div>
       </div>
     </Link>
   );
 };
 
-export default EmptyCartCard;
+export default EmptyFavCard;
--- src/favourite/pages/Favourite.js
+++ src/favourite/pages/Favourite.js
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
 import Section from "../../shared/UIElements/Section";
+import EmptyFavCard from "../components/EmptyFavCard";
 import FavItem from "../components/FavItem";
 import classes from "./Favourite.module.css";
 
 const Favourite = () => {
   const favouriteItems = useSelector((state) => state.products.favourites);
+  const favouritesIsEmpty =
+    useSelector((state) => state.products.favourites.length) === 0;
 
   return (
     <Section name="favourite">
@@ -20,6 +23,7 @@ const Favourite = () => {
             }}
           />
         ))}
+        {favouritesIsEmpty && <EmptyFavCard />}
       </div>
     </Section>
   );
