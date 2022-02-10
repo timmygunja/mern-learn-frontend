16f9f4fa59adc47d9aca17eec4eca916a6b5d694
timmygunja <t.urusov13@mail.ru>
2022-02-10 11:46:30 +0300

    Added RecommendSlider

--- src/home/components/RecommendSlider.js
+++ src/home/components/RecommendSlider.js
@@ -1,16 +1,82 @@
 import { useState } from "react";
 import classes from "./RecommendSlider.module.css";
 import RecommendSliderList from "./RecommendSliderList";
+import SelectBoxHorizontal from "./SelectBoxHorizontal";
+
+let currentIndex = 0;
+let leftConstraint = 0;
+let rightConstraint = 2;
 
 const RecommendSlider = (props) => {
-  const [clicked, setClicked] = useState(false);
+  const [leftButtonHidden, setLeftButtonHidden] = useState(true);
+  const [rightButtonHidden, setRightButtonHidden] = useState(false);
+  const [scrollAmount, setScrollAmount] = useState(0);
+  const style = { transform: `translateX(${scrollAmount}vw)` };
+
+  const leftArrowHandler = (event) => {
+    event.preventDefault();
+    setScrollAmount(scrollAmount + 28);
+    currentIndex -= 1;
+    if (currentIndex === leftConstraint) {
+      setLeftButtonHidden(true);
+    }
+    if (currentIndex <= rightConstraint) {
+      setRightButtonHidden(false);
+    }
+  };
+
+  const rightArrowHandler = (event) => {
+    event.preventDefault();
+    setScrollAmount(scrollAmount - 28);
+    currentIndex += 1;
+    if (currentIndex > leftConstraint) {
+      setLeftButtonHidden(false);
+    }
+    if (currentIndex >= rightConstraint) {
+      setRightButtonHidden(true);
+    }
+  };
 
   return (
-    <div className={classes["slider-content"]}>
-      <button onClick={() => {
-            setClicked(true)
-        }}>button</button>
-      <RecommendSliderList products={props.products} />
+    <div className={classes["slider"]}>
+      <div className={classes["tool-box"]}>
+        <div className={classes["group-box"]}>
+          {/* <select className={classes["select-box"]}>
+            <option>Popular</option>
+            <option>Newest</option>
+            <option>Oldest</option>
+            <option>Most liked</option>
+          </select> */}
+          <SelectBoxHorizontal />
+        </div>
+        <div className={classes["button-box"]}>
+          <button
+            className={
+              leftButtonHidden ? classes["btn-hidden"] : classes["arrow-button"]
+            }
+            onClick={leftArrowHandler}
+          >
+            {"<"}
+          </button>
+          <button
+            className={
+              rightButtonHidden
+                ? classes["btn-hidden"]
+                : classes["arrow-button"]
+            }
+            onClick={rightArrowHandler}
+          >
+            {">"}
+          </button>
+        </div>
+      </div>
+      <div className={classes["slider-content"]}>
+        <RecommendSliderList
+          products={props.products}
+          style={style}
+          className={classes["slider-list"]}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/home/components/SelectBoxHorizontal.js
@@ -0,0 +1,55 @@
+import { useRef, useState } from "react";
+import classes from "./SelectBoxHorizontal.module.css";
+
+const SelectBoxHorizontal = (props) => {
+  const [chosenOption, setChosenOption] = useState("Popular");
+  const [selectClass, setSelectClass] = useState("select-option-hidden");
+  const [arrowClass, setArrowClass] = useState("select-arrow-hidden");
+
+  const showOptionHandler = (props) => {
+    if (selectClass === "select-option") {
+      setSelectClass("select-option-hidden");
+    } else {
+      setSelectClass("select-option");
+    }
+    if (arrowClass === "select-arrow-hidden") {
+      setArrowClass("select-arrow-open");
+    } else {
+      setArrowClass("select-arrow-hidden");
+    }
+  };
+
+  const optionHandler = (props) => {
+    setChosenOption(props.target.innerText);
+    setSelectClass("select-option-hidden");
+    setArrowClass("select-arrow-hidden");
+  };
+
+  return (
+    <div className={classes["outer-box"]}>
+      <div
+        className={classes["select-option-chosen"]}
+        onClick={showOptionHandler}
+      >
+        <h4 className={classes["select-option-text"]}>{chosenOption}</h4>
+        <div className={classes[arrowClass]}>
+          <img src="select arrow.svg" />
+        </div>
+      </div>
+      <div className={classes[selectClass]} onClick={optionHandler}>
+        Popular
+      </div>
+      <div className={classes[selectClass]} onClick={optionHandler}>
+        Newest
+      </div>
+      <div className={classes[selectClass]} onClick={optionHandler}>
+        Liked
+      </div>
+      <div className={classes[selectClass]} onClick={optionHandler}>
+        Season
+      </div>
+    </div>
+  );
+};
+
+export default SelectBoxHorizontal;
