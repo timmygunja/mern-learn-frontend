bd4d5356c0c52836e06564895a02d5c0efddd349
timmygunja <t.urusov13@mail.ru>
2023-02-09 19:52:22 +0300

    deploy changes

--- src/env.js
+++ src/env.js
@@ -1,23 +1,12 @@
 // LOCAL
-// const MONGODB_USER = "admin";
-// const MONGODB_PASSWORD = "admin";
-// const MONGODB_URL =
-//   "mongodb+srv://admin:admin@cluster0.lwsbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // const PORT = 5000;
 // const BASE_URL = "http://localhost:" + PORT;
 
 // PUBLIC
-const MONGODB_USER = "mern-timmygunja";
-const MONGODB_PASSWORD = "Erma3eomTcvdd5Fv";
-const MONGODB_URL =
-  "mongodb+srv://mern-timmygunja:Erma3eomTcvdd5Fv@mern-timmygunja.gillkha.mongodb.net/?retryWrites=true&w=majority";
-const PORT = 5000;
+// const PORT = 5000;
 const BASE_URL = "https://mern-timmygunja-api.vercel.app";
 
 const env = {
-  MONGODB_URL,
-  MONGODB_USER,
-  MONGODB_PASSWORD,
   BASE_URL,
 };
 
